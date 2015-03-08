e34dde94e372b694c516795ac29b947c7a161874
gcherry <gcherry2@gmail.com>
2015-03-08 10:27:51 -0400

    Created Base entity model

---
+++ LoLStats.Core/Models/Entities/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoLStats.Core.Models
+{
+    public class Entity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime ModifiedOn { get; set; }
+    }
+}
